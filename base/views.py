6062a74644f8aac4031cf1a6fd1411a70cdcfa20
Daniela2319 <frankvt04@gmail.com>
2024-09-19 14:56:30 -0300

    atualização na nova branch

---
+++ base/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from base.forms import CadastroForm
+
+
+
+# Create your views here.
+def inicio(request):
+    return render(request, 'inicio.html')
+
+
+def cadastro(request):
+    sucesso = False
+    form = CadastroForm(request.POST or None)
+    if form.is_valid():
+        sucesso = True
+        form.save()
+    contexto = {
+        'form': form,
+        'sucesso': sucesso
+    }
+    return render(request, 'cadastro.html', contexto)
+        
+    
+    
