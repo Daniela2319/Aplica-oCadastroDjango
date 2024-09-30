166bd771a0032d5ba0f06d875d44981ae1998332
Daniela2319 <frankvt04@gmail.com>
2024-09-30 15:56:22 -0300

    adicionando serializers

--- rest_api/views.py
+++ rest_api/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from cursos.models import Curso
+from rest_api.serializers import CursoModelSerializer
 
 # Create your views here.
 @api_view(['GET', 'POST'])
@@ -8,3 +11,7 @@ def hello_world(request):
   if request.method == 'POST':
     return Response({'message': f'HEllo World!, {request.data.get("name")}'})
   return Response({'hello': 'world Api'})
+
+class CursoModelViewSet(ModelViewSet):
+  queryset = Curso.objects.all() #conjunto de busca queryset
+  serializer_class = CursoModelSerializer #serializador
